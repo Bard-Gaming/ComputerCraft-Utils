4eedd00a892c8c08fef84661509434856fc0d82f
Bard-Gaming <christophe@dronne.fr>
2025-01-11 22:42:22 +0100

    [src/libs] button: Added the ability to change foreground color

--- src/libs/widgets/button.lua
+++ src/libs/widgets/button.lua
@@ -35,10 +35,13 @@ function wButton.create(name, x, y, onclick)
     button.name = name
 
     -- Colors:
+    button.defaultFg = string.rep("0", name_len)
+    button.activeFg = string.rep("0", name_len)
+
     button.defaultBg = string.rep("f", name_len)
     button.activeBg = string.rep("b", name_len)
-    
-    button.fg = string.rep("0", name_len)
+
+    button.fg = button.defaultFg
     button.bg = button.defaultBg
 
     -- Add Button to list of Widgets:
@@ -50,12 +53,13 @@ end
 
 -------- Widget Process -------
 function wButton.update(button)
+    if button.activeFrames == 0 then
+        button.fg = button.defaultFg
+        button.bg = button.defaultBg
+    end
+
     if button.activeFrames > 0 then
         button.activeFrames = button.activeFrames - 1
-
-        if button.activeFrames == 0 then
-            button.bg = button.defaultBg
-        end
     end
 end
 
@@ -74,21 +78,15 @@ function wButton.click(button)
 
     -- Update activeFrames and visuals, then call user onclick fnc
     button.activeFrames = 5
+    button.fg = button.activeFg
     button.bg = button.activeBg
     button.fnc(button)
 end
 
 -------- Widget Utility -------
-function wButton.setDefaultColor(button, color)
+local function colorToBlitStr(color, length)
     local colorChar = colors.toBlit(color)
-
-    button.defaultBg = string.rep(colorChar, string.len(button.name))
-end
-
-function wButton.setActiveColor(button, color)
-    local colorChar = colors.toBlit(color)
-
-    button.activeBg = string.rep(colorChar, string.len(button.name))
+    return string.rep(colorChar, length)
 end
 
 function wButton.setName(button, name)
@@ -103,6 +101,12 @@ function wButton.setName(button, name)
     button.fg = string.rep(string.sub(button.fg, 1, 1), name_len)
     button.bg = string.rep(string.sub(button.bg, 1, 1), name_len)
 end
+
+-- Button Colors:
+function wButton.setDefaultFGColor(button, color) button.defaultFg = colorToBlitStr(color, string.len(button.name)) end
+function wButton.setActiveFGColor(button, color) button.activeFg = colorToBlitStr(color, string.len(button.name)) end
+function wButton.setDefaultBGColor(button, color) button.defaultBg = colorToBlitStr(color, string.len(button.name)) end
+function wButton.setActiveBGColor(button, color) button.activeBg = colorToBlitStr(color, string.len(button.name)) end
 
 
 return wButton
