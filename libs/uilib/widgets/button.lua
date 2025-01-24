754f260bf4316a18a1be7fd87c396b77ca7140f8
Bard-Gaming <christophe@dronne.fr>
2025-01-24 22:16:39 +0100

    [libs/uilib] Updated button implementation to be more consistent on the naming convention

--- libs/uilib/widgets/button.lua
+++ libs/uilib/widgets/button.lua
@@ -17,35 +17,35 @@ local Button = Widget:new("button")
 
 ------- Button Creation -------
 function Button:new(name, x, y, onclick)
-    local name_len = string.len(name)
-    local new_button = {}
+    local nameLen = string.len(name)
+    local newButton = {}
 
-    setmetatable(new_button, self)
+    setmetatable(newButton, self)
     self.__index = self
 
-    new_button.name = name
-    new_button.onClick = onclick or function() end
+    newButton.name = name
+    newButton.onClick = onclick or function() end
 
     -- Position:
-    new_button.startX = x
-    new_button.startY = y
-    new_button.endX = x + string.len(name)
-    new_button.endY = y
+    newButton.startX = x
+    newButton.startY = y
+    newButton.endX = x + string.len(name)
+    newButton.endY = y
 
     -- Colors:
-    new_button.defaultFg = string.rep("0", name_len)
-    new_button.defaultBg = string.rep("f", name_len)
+    newButton.defaultFg = string.rep("0", nameLen)
+    newButton.defaultBg = string.rep("f", nameLen)
 
-    new_button.activeFg = string.rep("0", name_len)
-    new_button.activeBg = string.rep("b", name_len)
+    newButton.activeFg = string.rep("0", nameLen)
+    newButton.activeBg = string.rep("b", nameLen)
 
-    new_button.fg = new_button.defaultFg
-    new_button.bg = new_button.defaultBg
+    newButton.fg = newButton.defaultFg
+    newButton.bg = newButton.defaultBg
 
     -- Misc. Data:
-    new_button.activeFrames = 0
+    newButton.activeFrames = 0
 
-    return new_button
+    return newButton
 end
 
 
@@ -90,25 +90,25 @@ local function colorToBlitStr(color, str)
 end
 
 function Button:setName(name)
-    local old_len = string.len(self.name)
-    local new_len = string.len(name)
+    local oldLen = string.len(self.name)
+    local newLen = string.len(name)
 
     self.name = name
 
     -- Colors only need to be updated if there is a difference in length
-    if old_len == new_len then
+    if oldLen == newLen then
         return
     end
 
     -- Update colors
-    self.defaultFg = string.rep(string.sub(self.defaultFg, 1, 1), new_len)
-    self.defaultBg = string.rep(string.sub(self.defaultBg, 1, 1), new_len)
+    self.defaultFg = string.rep(string.sub(self.defaultFg, 1, 1), newLen)
+    self.defaultBg = string.rep(string.sub(self.defaultBg, 1, 1), newLen)
 
-    self.activeFg = string.rep(string.sub(self.activeFg, 1, 1), new_len)
-    self.activeBg = string.rep(string.sub(self.activeBg, 1, 1), new_len)
+    self.activeFg = string.rep(string.sub(self.activeFg, 1, 1), newLen)
+    self.activeBg = string.rep(string.sub(self.activeBg, 1, 1), newLen)
 
-    self.fg = string.rep(string.sub(self.fg, 1, 1), new_len)
-    self.bg = string.rep(string.sub(self.bg, 1, 1), new_len)
+    self.fg = string.rep(string.sub(self.fg, 1, 1), newLen)
+    self.bg = string.rep(string.sub(self.bg, 1, 1), newLen)
 end
 
 function Button:setDefaultFGColor(color) self.defaultFg = colorToBlitStr(color, self.name) end
