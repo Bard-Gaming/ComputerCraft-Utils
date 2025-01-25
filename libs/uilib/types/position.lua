515a111d22a044327ec9dee172259041fe1ef1df
Bard-Gaming <christophe@dronne.fr>
2025-01-25 23:09:57 +0100

    [libs/uilib] types/position.lua: Updated Position to take a width and height value instead of start and end positions

--- libs/uilib/types/position.lua
+++ libs/uilib/types/position.lua
@@ -18,17 +18,17 @@ local Position = {
 
 
 ------- Widget Creation -------
-function Position:new(startX, startY, endX, endY)
+function Position:new(x, y, width, height)
     local newPosition = {}
 
     setmetatable(newPosition, self)
     self.__index = self
 
     -- Add Position Data:
-    newPosition.startX = startX
-    newPosition.startY = startY
-    newPosition.endX = endX
-    newPosition.endY = endY
+    newPosition.startX = x or 0
+    newPosition.startY = y or 0
+    newPosition.endX = newPosition.startX + (width or 0)
+    newPosition.endY = newPosition.startY + (height or 0)
     newPosition.alignment = "left"
 
     return newPosition
@@ -40,6 +40,29 @@ function Position:isInside(x, y)
     return
         self.startX <= x and x <= self.endX and
         self.startY <= y and y <= self.endY
+end
+
+function Position:setAlignment(alignment)
+    local alignmentTable = {
+        left = self.alignLeft,
+        center = self.alignCenter,
+        right = self.alignRight,
+    }
+
+    alignmentTable[alignment or "left"](self)  -- call corresponding alignment function
+end
+
+function Position:setSize(width, height)
+    local alignment = self.alignment
+    self:alignLeft()  -- (temporarily) reset the alignment
+
+    self.endX = self.startX + width
+
+    if height ~= nil then
+        self.endY = self.startY + height
+    end
+
+    self:setAlignment(alignment)
 end
 
 
@@ -66,7 +89,9 @@ function Position:alignCenter()
     if self.alignment == "center" then return end
     self:alignLeft()  -- reset alignment
 
+    local width = self.endX - self.startX
     local offset = math.floor(width / 2)
+
     self.startX = self.startX - offset
     self.endX = self.endX - offset
 
@@ -77,8 +102,10 @@ function Position:alignRight()
     if self.alignment == "right" then return end
     self:alignLeft()  -- reset alignment
 
+    local width = self.endX - self.startX
+
     self.endX = self.startX
-    self.startX = self.startX - self.width
+    self.startX = self.startX - width
 
     self.alignment = "right"
 end
